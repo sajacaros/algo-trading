19e9eb0d11c2c8b002f2ed5aab9f4a0087a606e4
dukim <dukim@bnpinnovation.com>
2022-05-27 17:39:10 +0900

    CoinInstrument

---
+++ lib/CoinInstrument.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import pyupbit
+import matplotlib.pyplot as plt
+import datetime
+import pytz
+
+
+class CoinInstrument:
+    KST = datetime.timezone(datetime.timedelta(hours=9))
+
+    def __init__(self, ticker, to=None, count=200, interval='day', period=0.5):
+        self._data = None
+        self.ticker = ticker
+        self.interval = interval
+        self.count = count
+        self.to = to
+        self.period = period
+        self.load_data()
+        self.log_returns()
+
+    def __repr__(self):
+        return "CoinInstrument(ticker={}, to={}, count={}, interval={})".format(self.ticker, self.to,
+                                                                                self.count, self.interval)
+
+    # def to_origin(self):
+    #     return self._utc_to_kst(self._to)
+    #
+    # def _kst_to_utc(self, to):
+    #     return pd.to_datetime(to).to_pydatetime().replace(tzinfo=CoinInstrument.KST).astimezone(
+    #         datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+    #
+    # def _utc_to_kst(self, to):
+    #     return pd.to_datetime(to).to_pydatetime().replace(tzinfo=datetime.timezone.utc).astimezone(
+    #         CoinInstrument.KST).strftime("%Y-%m-%d %H:%M:%S")
+
+    def load_data(self):
+        self._data = pyupbit.get_ohlcv(self.ticker, self.interval, self.count, self.to, self.period)
+        # self._data.index = self._data.index.tz_convert(None).tz_localize('Asia/Seoul')
+
+    def log_returns(self):
+        self._data["log_returns"] = np.log(self._data.close / self._data.close.shift(1))
+
+    def plot_prices(self, figsize=(12, 8)):
+        self._data.close.plot(figsize=figsize)
+        plt.title("Close chart : {}".format(self.ticker), fontsize=15)
+
+    def plot_returns(self, kind="ts", figsize=(12, 8)):
+        if kind == "ts":
+            self._data.log_returns.plot(figsize=figsize)
+            plt.title("Returns : {}".format(self.ticker), fontsize=15)
+        elif kind == "hist":
+            self._data.log_returns.hist(figsize=figsize, bins=int(np.sqrt(len(self._data))))
+            plt.title("Frequency of Returns : {}".format(self.ticker), fontsize=15)
+
+    def head(self, n=5):
+        return self._data.head(n)
+
+    def tail(self, n=5):
+        return self._data.tail(n)
+    
+    def sma(self, window):
+        return self._data.close.rolling(window).mean()
